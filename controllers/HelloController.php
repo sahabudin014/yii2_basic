b6d92b52d4e73f293bec99a69ab89f306353f758
sahabudin014 <sahaluai@gmail.com>
2017-05-23 12:22:33 +0700

    new controller

---
+++ controllers/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+
+class HelloController extends \yii\web\controller {
+
+public function actionIndex(){
+  return $this->render('index');
+}
+
+
+}
+
+ ?>
---
+++ views/hello/index.php
@@ -0,0 +1,1 @@
+<h1>Sahabudin</h1>
